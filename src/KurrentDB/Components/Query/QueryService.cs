0739aace40445e45077e645c6ee9998d6038d141
Alexey Zimarev <alex@zimarev.com>
2025-09-29 18:28:34 +0200

    * Added event_type to ad-hoc query (#5290)
    * Fixed query check to avoid false positives when parsing for validation

--- src/KurrentDB/Components/Query/QueryService.cs
+++ src/KurrentDB/Components/Query/QueryService.cs
@@ -15,49 +15,52 @@ using Serilog;
 namespace KurrentDB.Components.Query;
 
 public static partial class QueryService {
-	public static string AmendQuery(string query) {
+	private static string AmendQuery(DuckDBConnectionPool pool, string query) {
 		var matches = ExtractionRegex().Matches(query);
 		List<string> ctes = [AllCte];
 		foreach (Match match in matches) {
 			if (!match.Success) continue;
 			var tokens = match.Value.Split(':');
 			var cteName = ReplaceSpecialCharsWithUnderscore($"{tokens[0]}_{tokens[1]}");
-			var where = "where " + tokens[0] switch {
+			var where = $"where {tokens[0] switch {
 				"stream" => $"stream = '{tokens[1]}'",
 				"category" => $"category = '{tokens[1]}'",
 				_ => throw new("Invalid token")
-			};
+			}}";
 			var cte = string.Format(CteTemplate, cteName, where);
 			ctes.Add(cte);
 			query = query.Replace(match.Value, cteName);
 		}
 
+		ValidateQuery(pool, query);
 		return $"with\r\n{string.Join(",\r\n", ctes)}\r\n{query}";
 	}
 
-	internal static List<Dictionary<string, object>> ExecuteAdHocUserQuery(this DuckDBConnectionPool pool, string sql) {
-		var result = pool.QueryFirstOrDefault<Sql2Json.Args, Sql2Json.Result, Sql2Json>(new(sql.ReplaceLineEndings("")));
-		if (!result.HasValue) {
+	private static void ValidateQuery(DuckDBConnectionPool pool, string query) {
+		// var result = pool.QueryFirstOrDefault<Sql2Json.Args, Sql2Json.Result, Sql2Json>(new(query));
+		using var _ = pool.Rent(out var connection);
+		var result = connection.QueryFirstOrDefault<string>("select json_serialize_sql($query::varchar)", new { query });
+		if (result == null) {
 			throw new("Error parsing query");
 		}
-
-		var conversionResponse = JsonSerializer.Deserialize<SqlJsonResponse>(result.Value.Json);
+		var conversionResponse = JsonSerializer.Deserialize<SqlJsonResponse>(result);
 		if (conversionResponse.Error) {
 			var error = conversionResponse.ErrorMessage.StartsWith("Only SELECT")
 				? "Only SELECT statements are allowed"
 				: $"Error parsing query: {conversionResponse.ErrorMessage}";
 			throw new(error);
 		}
+	}
 
+	internal static List<Dictionary<string, object>> ExecuteAdHocUserQuery(this DuckDBConnectionPool pool, string sql) {
+		var query = AmendQuery(pool, sql);
 		using var scope = pool.Rent(out var connection);
-		var query = AmendQuery(sql);
 		var items = (IEnumerable<IDictionary<string, object>>)connection.Query(query);
 		return items.Select(x => x.ToDictionary(y => y.Key, y => y.Value)).ToList();
 	}
 
-	private static string ReplaceSpecialCharsWithUnderscore(string input) {
-		return string.IsNullOrEmpty(input) ? string.Empty : SpecialCharsRegex().Replace(input, "_");
-	}
+	private static string ReplaceSpecialCharsWithUnderscore(string input)
+		=> string.IsNullOrEmpty(input) ? string.Empty : SpecialCharsRegex().Replace(input, "_");
 
 	[GeneratedRegex(@"\b(?:stream|category):([A-Za-z0-9_-]+)\b", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant)]
 	private static partial Regex ExtractionRegex();
@@ -69,13 +72,13 @@ public static partial class QueryService {
 
 	private const string CteTemplate = """
 	                                   {0} AS (
-	                                       select log_position, stream, event_number, epoch_ms(created) as created_at, event->>'data' as data, event->>'metadata' as metadata
+	                                       select log_position, stream, event_number, event_type, epoch_ms(created) as created_at, event->>'data' as data, event->>'metadata' as metadata
 	                                       from (
 	                                           select *, kdb_get(log_position)::JSON as event
 	                                           from (
-	                                               select stream, event_number, log_position, created from idx_all {1}
+	                                               select stream, event_number, event_type, log_position, created from idx_all {1}
 	                                               union all
-	                                               select stream, event_number, log_position, created from inflight() {1}
+	                                               select stream, event_number, event_type, log_position, created from inflight() {1}
 	                                           )
 	                                       )
 	                                   )
@@ -83,8 +86,9 @@ public static partial class QueryService {
 
 	private record SqlJsonResponse {
 		[JsonPropertyName("error")] public bool Error { get; init; }
-
 		[JsonPropertyName("error_message")] public string ErrorMessage { get; init; } = "";
+		[JsonPropertyName("error_subtype")] public string ErrorSubtype { get; init; } = "";
+		[JsonPropertyName("position")]  public string Position { get; init; }
 	}
 
 	public struct Sql2Json : IQuery<Sql2Json.Args, Sql2Json.Result> {

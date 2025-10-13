b26080a050e6eb5592af23d6f0722557a91514a9
William Chong <william-chong@outlook.com>
2025-10-13 15:08:35 +0400

    [DEV-957] Implement System readiness in DuckDBProjector service (#5309)
    * test(registry): projector service runs on all nodes
    
    * Implement wait for system ready
    
    * Fixup test
    
    * Revert changes
    
    * implement system readiness handling in DuckDBProjectorService
    
    * Fixup

--- src/SchemaRegistry/KurrentDB.SchemaRegistry/Planes/Projection/DuckDBProjectorService.cs
+++ src/SchemaRegistry/KurrentDB.SchemaRegistry/Planes/Projection/DuckDBProjectorService.cs
@@ -5,15 +5,18 @@ using Kurrent.Surge.Consumers.Configuration;
 using Kurrent.Surge.DuckDB;
 using Kurrent.Surge.DuckDB.Projectors;
 using KurrentDB.Core.Bus;
+using KurrentDB.Core.Messages;
 using KurrentDB.SchemaRegistry.Data;
 using KurrentDB.SchemaRegistry.Infrastructure.System.Node;
 
 namespace KurrentDB.SchemaRegistry.Planes.Projection;
 
 public class DuckDBProjectorService(
-    IPublisher publisher, IDuckDBConnectionProvider connectionProvider, IConsumerBuilder consumerBuilder, ILoggerFactory loggerFactory)
-    : NodeBackgroundService(publisher, loggerFactory.CreateLogger<NodeBackgroundService>(), "DuckDBProjector") {
+    IPublisher publisher, ISubscriber subscriber, IDuckDBConnectionProvider connectionProvider, IConsumerBuilder consumerBuilder, ILoggerFactory loggerFactory)
+    : DuckDBProjectorBackgroundService(publisher, subscriber, loggerFactory.CreateLogger<DuckDBProjectorBackgroundService>(), "DuckDBProjector") {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken) {
+        await WaitForSystemReady(stoppingToken);
+
         var options = new DuckDBProjectorOptions(connectionProvider) {
             Filter          = SchemaRegistryConventions.Filters.SchemasFilter,
             InitialPosition = SubscriptionInitialPosition.Latest,
@@ -31,4 +34,34 @@ public class DuckDBProjectorService(
 
         await projector.RunUntilStopped(stoppingToken);
     }
+}
+
+// TODO: Refactor to ensure both connector and registry use a unified system readiness component
+public class DuckDBProjectorBackgroundService : NodeBackgroundService, IHandle<SystemMessage.SystemReady> {
+	readonly TaskCompletionSource _systemReady = new();
+
+	public DuckDBProjectorBackgroundService(IPublisher publisher, ISubscriber subscriber, ILogger<NodeBackgroundService> logger, string? serviceName = null) : base(publisher, logger, serviceName) {
+		Logger = logger;
+		subscriber.Subscribe(this);
+	}
+
+    protected ILogger Logger { get; }
+
+	public void Handle(SystemMessage.SystemReady message) {
+		_systemReady.TrySetResult();
+	}
+
+	protected async Task WaitForSystemReady(CancellationToken cancellationToken) {
+		await _systemReady.Task.WaitAsync(cancellationToken);
+		Logger.LogDuckDBProjectorSystemReady(ServiceName);
+	}
+
+	protected override Task ExecuteAsync(CancellationToken stoppingToken) {
+		return Task.CompletedTask;
+	}
+}
+
+static partial class DuckDBProjectorBackgroundServiceLogMessages {
+	[LoggerMessage(LogLevel.Debug, "{ServiceName} system is ready")]
+	internal static partial void LogDuckDBProjectorSystemReady(this ILogger logger, string serviceName);
 }
